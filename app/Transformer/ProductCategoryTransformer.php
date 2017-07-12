82a8e2ae62fe9bf21353956249f2fc5f48972830
Daniel Hermawan <danielhermawan96@gmail.com>
2017-07-12 18:51:39 +0700

    Fix transformer to not include product

--- app/Transformer/ProductCategoryTransformer.php
+++ app/Transformer/ProductCategoryTransformer.php
@@ -11,8 +11,8 @@ use League\Fractal\TransformerAbstract;
  */
 class ProductCategoryTransformer extends TransformerAbstract
 {
-    protected $availableIncludes = ['products'];
-    protected $defaultIncludes = ['products'];
+    protected $availableIncludes = [];
+    protected $defaultIncludes = [];
 
     public function transform($category): array
     {
@@ -20,11 +20,5 @@ class ProductCategoryTransformer extends TransformerAbstract
             "id" => $category->id,
             "name" => $category->name
         ];
-    }
-
-    public function includeProducts($category)
-    {
-        $products = $category->products;
-        return $this->collection($products, new UserProductTransformer());
     }
 }
